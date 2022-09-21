b11cc3329612987c594b0fc4c28e4a98bcec69c3
Timothy Koh <tiko@microsoft.com>
2022-09-21 10:10:16 -0700

    Working pin selections to get button going

--- targets/project-janus/profile/module.c
+++ targets/project-janus/profile/module.c
@@ -9,7 +9,8 @@ FIRMWARE_IDENTIFIER(0x30249c44, "Project Janus XBox Gateway");
 void app_init_services() {
     // see jacdac-c/services/jd_services.h for the services that can be
     // enabled here; you can enable zero or more services
-    button_init(PA_5, 0, NO_PIN);
+    button_init(PC_13, 0, NO_PIN);
+	// hidkeyboard_init();
 
     example_custom_function(); // call our custom function
 }
